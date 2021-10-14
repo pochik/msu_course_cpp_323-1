7cd1367f1b3861174aeca5ae1317b3125b3d7a38
pochik <nikolay@shapurov.ru>
2021-10-14 10:15:44 +0300

    formatting

--- task_02/nikolay_shapurov/graph.cpp
+++ task_02/nikolay_shapurov/graph.cpp
@@ -1,22 +1,22 @@
 #include <array>
+#include <cassert>
 #include <fstream>
 #include <iostream>
 #include <vector>
-#include <cassert>
 
 constexpr int INVALID_ID = -1;
 
 class Graph {
-private:
+ private:
   int num_vertices, num_edges;
   std::vector<std::vector<int>> data;
 
-public:
-
+ public:
   Graph(int num_v, int num_e) {
-
-    assert((num_v >= 0) && "Invalid number of vertices. Must be positive integer.");
-    assert((num_e >= 0) && "Invalid number of edges. Must be positive integer.");
+    assert((num_v >= 0) &&
+           "Invalid number of vertices. Must be positive integer.");
+    assert((num_e >= 0) &&
+           "Invalid number of edges. Must be positive integer.");
 
     num_vertices = num_v;
     num_edges = num_e;
@@ -29,14 +29,15 @@ public:
     }
   }
 
-  void add_edge(int edge_id, int vertex_id1, int vertex_id2){
+  void add_edge(int edge_id, int vertex_id1, int vertex_id2) {
+    assert((edge_id >= 0) && "Ivalid edge ID. Must be positive integer.");
+    assert((vertex_id1 >= 0) &&
+           "Invalid the first vertex ID. Must be positive integer.");
+    assert((vertex_id2 >= 0) &&
+           "Invalid the second vertex ID. Must be positive integer.");
 
-      assert((edge_id >= 0) && "Ivalid edge ID. Must be positive integer.");
-      assert((vertex_id1 >= 0) && "Invalid the first vertex ID. Must be positive integer.");
-      assert((vertex_id2 >= 0) && "Invalid the second vertex ID. Must be positive integer.");
-
-      data[vertex_id1][vertex_id2] = edge_id;
-      data[vertex_id2][vertex_id1] = edge_id;
+    data[vertex_id1][vertex_id2] = edge_id;
+    data[vertex_id2][vertex_id1] = edge_id;
   }
 
   void Print() {
@@ -48,59 +49,58 @@ public:
     }
   }
 
-  void PrintJSON(){
+  void PrintJSON() {
+    std::ofstream fout;
+    fout.open("output.json");
 
-      std::ofstream fout;
-      fout.open("output.json");
+    fout << "{\n    \"vertices\": [\n";
+    std::vector<std::vector<int>> edges;
 
-      fout << "{\n    \"vertices\": [\n";
-      std::vector<std::vector<int>> edges;
+    for (int i = 0; i <= num_edges; i++) {
+      std::vector<int> tmp;
+      edges.push_back(tmp);
+      edges[i].push_back(0);
+      edges[i].push_back(0);
+    }
 
-      for (int i = 0; i <= num_edges; i++){
-          std::vector<int> tmp;
-          edges.push_back(tmp);
-          edges[i].push_back(0);
-          edges[i].push_back(0);
-      }
-
-      for (int i = 0; i < num_vertices; i++) {
-        fout << "     {\n        \"id\": " << i << ",\n        \"edge_ids\": [";
-        bool first_semicolon = 1;
-        for (int j = 0; j < num_vertices; j++) {
-          if (data[i][j] != -1) {
-            if (first_semicolon == 0) {
-              fout << ", " << data[i][j];
-            } else {
-              first_semicolon = 0;
-              fout << data[i][j];
-            }
-            edges[data[i][j]][0] = i;
-            edges[data[i][j]][1] = j;
+    for (int i = 0; i < num_vertices; i++) {
+      fout << "     {\n        \"id\": " << i << ",\n        \"edge_ids\": [";
+      bool first_semicolon = 1;
+      for (int j = 0; j < num_vertices; j++) {
+        if (data[i][j] != -1) {
+          if (first_semicolon == 0) {
+            fout << ", " << data[i][j];
+          } else {
+            first_semicolon = 0;
+            fout << data[i][j];
           }
-        }
-        if (i == num_vertices - 1) {
-          fout << "]\n      }\n ";
-        } else {
-          fout << "]\n      },\n ";
+          edges[data[i][j]][0] = i;
+          edges[data[i][j]][1] = j;
         }
       }
-      fout << "        \n    ],\n";
-
-      fout << "\n    \"edges\": [\n";
-      for (int i = 0; i < num_edges; i++) {
-        if (i == num_edges - 1) {
-          fout << "      {\n        \"id\": " << i << ",\n        \"vertex_ids\": ["
-               << edges[i][0] << ", " << edges[i][1] << "]\n      } \n";
-        } else {
-          fout << "      {\n        \"id\": " << i << ",\n        \"vertex_ids\": ["
-               << edges[i][0] << ", " << edges[i][1] << "]\n      }, \n";
-        }
+      if (i == num_vertices - 1) {
+        fout << "]\n      }\n ";
+      } else {
+        fout << "]\n      },\n ";
       }
-      fout << "        \n    ]\n}\n\n";
-      fout.close();
+    }
+    fout << "        \n    ],\n";
 
+    fout << "\n    \"edges\": [\n";
+    for (int i = 0; i < num_edges; i++) {
+      if (i == num_edges - 1) {
+        fout << "      {\n        \"id\": " << i
+             << ",\n        \"vertex_ids\": [" << edges[i][0] << ", "
+             << edges[i][1] << "]\n      } \n";
+      } else {
+        fout << "      {\n        \"id\": " << i
+             << ",\n        \"vertex_ids\": [" << edges[i][0] << ", "
+             << edges[i][1] << "]\n      }, \n";
+      }
+    }
+    fout << "        \n    ]\n}\n\n";
+    fout.close();
   }
-
 };
 
 int main() {
